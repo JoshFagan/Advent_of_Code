0720a1cb07b5f833bb6c87a8b045966b72cd0f2a
Joshua Fagan <jfagan2@vols.utk.edu>
2021-12-02 11:47:45 -0500

    Implements second half

--- 2021/Day_1/solution.py
+++ 2021/Day_1/solution.py
@@ -11,12 +11,25 @@ def load_data():
     return contents 
 
 
+def first_half(depths):
+    incs = [depths[i-1] < depths[i] for i in range(1,len(depths))]
+
+    print('\nSolution for first half!')
+    print('Number of increasing measurements: {}\n'.format(sum(incs)))
+
+
+def second_half(depths):
+    windows = [depths[i-2] + depths[i-1] + depths[i] for i in range(2,len(depths))]
+    incs = [windows[i-1] < windows[i] for i in range(1,len(windows))]
+    
+    print('Solution for second half!')
+    print('Number of increasing measurements: {}\n'.format(sum(incs)))
+
+
 def main():
     depths = load_data() 
-    
-    diffs = [depths[i-1] < depths[i] for i in range(1,len(depths))]
-
-    print('Number of increasing measurements: {}'.format(sum(diffs)))
+    first_half(depths)
+    second_half(depths)
 
 
 if __name__ == '__main__':
