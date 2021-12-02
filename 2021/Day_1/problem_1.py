499437f254f70450ffb84792b945293c93bcd683
Joshua Fagan <jfagan2@vols.utk.edu>
2021-12-02 11:25:44 -0500

    Moves loading data to separate function

--- 2021/Day_1/problem_1.py
+++ 2021/Day_1/problem_1.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
 
 
-def main():
+def load_data():
     # Read contents of file
     my_file = open('depth_measurement_data.txt', 'r')
     contents = my_file.readlines()
     my_file.close()
-    depths = [int(content) for content in contents]
+    contents = [int(content) for content in contents]
+
+    return contents 
+
+
+def main():
+    depths = load_data() 
 
 
 if __name__ == '__main__':
