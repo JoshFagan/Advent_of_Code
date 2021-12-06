e35a7f532013811c92b05982c231665e1d32cc7e
Joshua Fagan <jfagan2@vols.utk.edu>
2021-12-06 13:45:42 -0500

    Final solution to both halves

--- 2021/Day_4/solution.py
+++ 2021/Day_4/solution.py
@@ -17,33 +17,95 @@ def load_data():
 def process_data(raw_data):
     draw_numbers = [int(x) for x in raw_data[0].split(',')]
 
-    boards = np.array([[int(x) for x in row.split()] for row in raw_data[2:] if row if row != '\n'])
+    boards = np.array([[int(x) for x in row.split()] 
+                       for row in raw_data[2:] if row if row != '\n'])
 
     return (draw_numbers, boards) 
 
 
 def first_half(draw_numbers, boards): 
-    print(boards.shape)
+    # Set up mechanics to track scoring numbers
     num_boards = int(boards.shape[0]/5)
     row_counts = np.zeros((5, num_boards)) 
     col_counts = np.zeros((num_boards, 5))
+    # Iterate over all numbers
+    for num in draw_numbers:
+        indices = np.where(boards==num)
+        # Score all numbers
+        for i in range(len(indices[0])):
+            # Parse specifics about location of drawn number
+            game = int(indices[0][i]/5)
+            row = indices[0][i] % 5
+            col = indices[1][i]
 
-    print('\nSolution for first half!')
-    print('Product of gamma and epsilon: {}\n'.format(solution))
+            # Mark the position on the board as drawn
+            boards[indices[0][i]][indices[1][i]] = 0
 
+            # Update drawn row and column counts
+            row_counts[row][game] += 1
+            col_counts[game][col] += 1
 
-def second_half(report):
+            # End game if row or column counts for a game wins
+            if row_counts[row][game] >= 5 or col_counts[game][col] >= 5:
+                solution = sum(sum(boards[game*5:game*5+5])) * num
+                print('\nSolution for first half!')
+                print('Product of board sum and drawn number: {}\n'.format(
+                          solution))
+                return
+        
 
-    print('Solution for second half!')
-    print('Product of oxygen rating and co2 rating: {}\n'.format(solution))
+def second_half(draw_numbers, boards):
+    # Set up mechanics to track scoring numbers
+    num_boards = int(boards.shape[0]/5)
+    row_counts = np.zeros((5, num_boards)) 
+    col_counts = np.zeros((num_boards, 5))
+    won_boards = np.array([])
+    # Iterate over all numbers
+    for num in draw_numbers:
+        indices = np.where(boards==num)
+        # Score all numbers
+        for i in range(len(indices[0])):
+            # Parse specifics about location of drawn number
+            game = int(indices[0][i]/5)
+            row = indices[0][i] % 5
+            col = indices[1][i]
+            # If this game as already one, skip updating everthing
+            if game in won_boards:
+                continue
+
+            # Mark the position on the board as drawn
+            boards[indices[0][i]][indices[1][i]] = -1
+
+            # Update drawn row and column counts
+            row_counts[row][game] += 1
+            col_counts[game][col] += 1
+
+            # Check if a game board wins
+            if row_counts[row][game] >= 5 or col_counts[game][col] >= 5:
+                # End game if this is the last game board
+                if num_boards == 1:
+                    # Change drawn location markers to additive identity 
+                    winner = boards[game*5:game*5+5]
+                    winner[winner == -1] = 0
+
+                    solution = sum(sum(winner)) * num
+                    print('\nSolution for second half!')
+                    print('Product of board sum and drawn number: {}\n'.format(
+                              solution))
+                    return
+                else:
+                    # If this is not the last game, update counts and keeg going
+                    won_boards = np.append(won_boards, game)
+                    num_boards -= 1
+                    boards[game*5:game*5+5] = -1
 
 
 def main():
     bingo = load_data() 
     (draw_numbers, boards) = process_data(bingo)
-    print(boards)
     first_half(draw_numbers, boards)
-    second_half(bingo)
+    (draw_numbers, boards) = process_data(bingo)
+    second_half(draw_numbers, boards)
 
 
 if __name__ == '__main__':
