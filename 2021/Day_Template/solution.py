52114437cbe61c753d1768f2486d34527eecb737
Joshua Fagan <jfagan2@vols.utk.edu>
2021-12-06 16:11:39 -0500

    Creates template for days and starts day 6

---
+++ 2021/Day_Template/solution.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3.8
+
+import numpy as np
+import scipy
+from scipy import stats
+
+
+def load_data():
+    # Read contents of file
+    my_file = open('data.txt', 'r')
+    contents = my_file.readlines()
+    my_file.close()
+
+    return contents 
+
+
+def process_data(raw_data):
+    return 
+
+
+def first_half(input): 
+    solution = 'NOT DONE YET' 
+    print('\nSolution for first half!')
+    print('SOLUTION DESCRIPTION: {}\n'.format(solution))
+    return 
+        
+
+def second_half(input):
+    solution = 'NOT DONE YET' 
+    print('\nSolution for first half!')
+    print('SOLUTION DESCRIPTION: {}\n'.format(solution))
+    return
+
+
+def main():
+    data = load_data() 
+    process_data(data)
+    first_half(data)
+    second_half(data)
+
+
+if __name__ == '__main__':
+    main()
