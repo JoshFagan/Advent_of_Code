88bb8e95fdf50bd94aafe91a887e3633f0320c7e
Joshua Fagan <jfagan2@vols.utk.edu>
2021-12-06 15:54:02 -0500

    Completes solution for Day 5

--- 2021/Day_5/solution.py
+++ 2021/Day_5/solution.py
@@ -22,14 +22,60 @@ def process_data(raw_data):
 
 
 def first_half(start_poses, end_poses): 
-    solution = 'NOT DONE YET' 
+    # Create graph
+    max_row = max(max(start_poses[:,1]), 
+                max(end_poses[:,1]))
+    max_col = max(max(start_poses[:,0]), 
+                max(end_poses[:,0]))
+    graph = np.zeros((max_row+1, max_col+1))
+
+    # Add all lines to graph
+    for i in range(len(start_poses)):
+        row_a = start_poses[i,1]
+        row_b = end_poses[i,1]
+        col_a = start_poses[i,0]
+        col_b = end_poses[i,0]
+        # Only add lines that are horizontal or vertical
+        if row_a != row_b and col_a != col_b: 
+           continue
+
+        # Iterate over every row
+        for row in range(min(row_a, row_b), max(row_a, row_b)+1):
+            # Iterate over every column
+            for col in range(min(col_a, col_b), max(col_a, col_b)+1):
+                # Increment every element on line
+                graph[row][col] += 1
+
+    solution = sum(sum(graph>=2))
     print('\nSolution for first half!')
     print('Number of \"2 or higher\" locations: {}\n'.format(solution))
-    return
+    return graph
         
 
-def second_half(start_poses, end_poses):
-    solution = 'NOT DONE YET' 
+def second_half(start_poses, end_poses, graph):
+    # Add all lines to graph
+    for i in range(len(start_poses)):
+        row_a = start_poses[i,1]
+        row_b = end_poses[i,1]
+        col_a = start_poses[i,0]
+        col_b = end_poses[i,0]
+        # Only add lines that are diagonal with angle 45 
+        if col_a == col_b:
+            continue
+        if abs(row_b-row_a) / abs(col_b-col_a) != 1: 
+            continue
+
+        # Determine sign of slope of line
+        sign_row = int((row_b-row_a)/abs(row_b-row_a))
+        sign_col = int((col_b-col_a)/abs(col_b-col_a))
+
+        # Iterate over row and column simultaneously and increment graph
+        row = row_a
+        for col in range(col_a, col_b+sign_col, sign_col):
+            graph[row][col] += 1
+            row += sign_row
+
+    solution = sum(sum(graph>=2))
     print('\nSolution for first half!')
     print('Number of \"2 or higher\" locations: {}\n'.format(solution))
     return
@@ -38,8 +84,8 @@ def second_half(start_poses, end_poses):
 def main():
     vents = load_data() 
     start_poses, end_poses = process_data(vents)
-    first_half(start_poses, end_poses)
-    second_half(start_poses, end_poses)
+    graph = first_half(start_poses, end_poses)
+    second_half(start_poses, end_poses, graph)
 
 
 if __name__ == '__main__':
