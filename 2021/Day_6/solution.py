c94b73a478eceba0828ac0cd97ef85ab97cd18da
Joshua Fagan <jfagan2@vols.utk.edu>
2021-12-07 12:57:46 -0500

    Clean up after completion of problem

--- 2021/Day_6/solution.py
+++ 2021/Day_6/solution.py
@@ -15,28 +15,39 @@ def load_data():
 
 
 def process_data(raw_data):
-    return 
+    initial_state = [int(f) for f in raw_data[0].split(',')]
+    return initial_state 
 
 
-def first_half(input): 
-    solution = 'NOT DONE YET' 
-    print('\nSolution for first half!')
-    print('SOLUTION DESCRIPTION: {}\n'.format(solution))
+def solution(initial_state, days): 
+    # Initialize empty counts
+    fish_count = {-1:0, 0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0}
+    # Propogate counts with intitial state
+    for fish in initial_state:
+        fish_count[fish] = fish_count[fish] + 1
+
+    # Each Day
+    for day in range(days):
+        # Roll each fish count down one
+        for fish in range(9):
+            fish_count[fish-1] = fish_count[fish]
+        # Adjust for resetting fish
+        fish_count[6] += fish_count[-1]
+        fish_count[8] = fish_count[-1]
+            
+
+    fish_count[-1] = 0
+    solution = sum(fish_count.values())
+    print('\nSolution!')
+    print('How many lanternfish after {} days?: {}\n'.format(days, solution))
     return 
         
 
-def second_half(input):
-    solution = 'NOT DONE YET' 
-    print('\nSolution for first half!')
-    print('SOLUTION DESCRIPTION: {}\n'.format(solution))
-    return
-
-
 def main():
     data = load_data() 
-    process_data(data)
-    first_half(data)
-    second_half(data)
+    initial_state = process_data(data)
+    solution(initial_state, 80)
+    solution(initial_state, 256)
 
 
 if __name__ == '__main__':
