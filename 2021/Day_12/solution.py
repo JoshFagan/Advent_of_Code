e23bef0bfda887d590eb80cb00c62464bdacaafb
Joshua Fagan <jfagan2@vols.utk.edu>
2021-12-17 13:21:39 -0500

    Implements first half of problem

--- 2021/Day_12/solution.py
+++ 2021/Day_12/solution.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3.8
 
+import numpy as np
 import csv
 
 
@@ -11,12 +12,48 @@ def load_data():
     return data
 
 
-def process_data(raw_data):
-    return 
+def create_graph(connections):
+    graph = {key: set([]) for key in np.unique(connections)}
+    for c in connections:
+        graph[c[0]].add(c[1])
+        if c[0] != 'start' and c[1] != 'end':
+            graph[c[1]].add(c[0])
+
+    return graph
 
 
-def first_half(input): 
-    solution = 'NOT DONE YET' 
+def first_half_recursion(graph, cave, cave_path, small_caves, start_to_end):
+    # graph: Dictionary mapping caves (strings) to set of caves they connect to
+    # cave: String representing current cave
+    # cave_path: List of caves (strings) visited so far
+    # small_caves: Set of small caves (strings) that have been visited so far 
+    # start_to_end: List of cave lists (lists of strings) that go from start to 
+        # end without visiting small caves more than once
+
+    # Check if this is "end"
+    if cave == 'end':
+        cave_path.append(cave)
+        start_to_end.append(cave_path)
+        return
+
+    # Update path to current cave
+    cave_path.append(cave)
+
+    # Update list of small caves visited
+    if cave.islower():
+        small_caves.add(cave)
+
+    # Visit all viable caves
+    for child in graph[cave] - small_caves:
+        first_half_recursion(graph, child, cave_path[:], set(small_caves),
+                             start_to_end)
+
+
+def first_half(map): 
+    graph = create_graph(map)
+    solutions = []
+    first_half_recursion(graph, 'start', [], set([]), solutions)
+    solution = len(solutions) 
     print('\nSolution for first half!')
     print('SOLUTION DESCRIPTION: {}\n'.format(solution))
     return 
@@ -31,8 +68,6 @@ def second_half(input):
 
 def main():
     data = load_data() 
-    print(data)
-    process_data(data)
     first_half(data)
     second_half(data)
 
