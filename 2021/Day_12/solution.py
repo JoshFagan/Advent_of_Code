798d67a3a245b6e11cd8e6f1affaf9b9a3a2000d
Joshua Fagan <jfagan2@vols.utk.edu>
2021-12-28 11:31:45 -0500

    Progress towards solution for second half

--- 2021/Day_12/solution.py
+++ 2021/Day_12/solution.py
@@ -22,13 +22,19 @@ def create_graph(connections):
     return graph
 
 
-def first_half_recursion(graph, cave, cave_path, small_caves, start_to_end):
+def recursive_func(graph, cave, cave_path, small_caves, start_to_end, 
+                   visited_twice):
     # graph: Dictionary mapping caves (strings) to set of caves they connect to
     # cave: String representing current cave
     # cave_path: List of caves (strings) visited so far
     # small_caves: Set of small caves (strings) that have been visited so far 
     # start_to_end: List of cave lists (lists of strings) that go from start to 
         # end without visiting small caves more than once
+    print('====================')
+    print(cave)
+    print(cave_path)
+    print(small_caves)
+    print(visited_twice)
 
     # Check if this is "end"
     if cave == 'end':
@@ -40,27 +46,48 @@ def first_half_recursion(graph, cave, cave_path, small_caves, start_to_end):
     cave_path.append(cave)
 
     # Update list of small caves visited
-    if cave.islower():
+    if cave.islower() and not cave == 'start':
+        # Pass over this node to double tap
         small_caves.add(cave)
-
-    # Visit all viable caves
-    for child in graph[cave] - small_caves:
-        first_half_recursion(graph, child, cave_path[:], set(small_caves),
-                             start_to_end)
+        for child in graph[cave] - small_caves:
+            recursive_func(graph, child, cave_path[:], set(small_caves), 
+                           start_to_end, 0)
+        small_caves.remove(cave)
+        
+        # Double tap this node
+        if visited_twice: 
+            small_caves.add(cave)
+        else:
+            visited_twice = 1
+        for child in graph[cave] - small_caves:
+            recursive_func(graph, child, cave_path[:], set(small_caves), 
+                           start_to_end, visited_twice)
+    elif cave.isupper():
+        # Visit all viable caves
+        for child in graph[cave] - small_caves:
+            recursive_func(graph, child, cave_path[:], set(small_caves), 
+                           start_to_end, visited_twice)
 
 
 def first_half(map): 
     graph = create_graph(map)
     solutions = []
-    first_half_recursion(graph, 'start', [], set([]), solutions)
+    print(graph)
+    recursive_func(graph, 'start', [], set([]), solutions, 1)
     solution = len(solutions) 
     print('\nSolution for first half!')
     print('SOLUTION DESCRIPTION: {}\n'.format(solution))
     return 
         
 
-def second_half(input):
-    solution = 'NOT DONE YET' 
+def second_half(map):
+    graph = create_graph(map)
+    solutions = []
+    recursive_func(graph, 'start', [], set([]), solutions, 0)
+    print('-------------')
+    for sol in solutions:
+        print(sol)
+    solution = len(solutions) 
     print('\nSolution for first half!')
     print('SOLUTION DESCRIPTION: {}\n'.format(solution))
     return
