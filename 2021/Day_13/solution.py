cb14ca5a011a105da8baba9aaad6bc6996b37f37
Joshua Fagan <jfagan2@vols.utk.edu>
2021-12-28 11:31:00 -0500

    Adds printing of second half solution to file

--- 2021/Day_13/solution.py
+++ 2021/Day_13/solution.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3.8
 
 import csv
 import numpy as np 
@@ -45,8 +45,33 @@ def first_half(coords, folds):
     return 
         
 
-def second_half(input):
-    solution = 'NOT DONE YET' 
+def second_half(coords, folds):
+    for fold in folds:
+        new_coords = set([]) 
+        for coord in coords:
+            if fold[0] == 1:
+                new_x = coord[0]
+                new_y = min(coord[1], 2*fold[1]-coord[1])
+            if fold[0] == 0:
+                new_x = min(coord[0], 2*fold[1]-coord[0])
+                new_y = coord[1]
+    
+            new_coords.add((new_x, new_y))
+        coords = new_coords
+
+    max_x = max([c[0] for c in coords])
+    max_y = max([c[1] for c in coords])
+
+    display = np.zeros((max_y+1, max_x+1))
+
+    for coord in coords:
+        display[coord[1],coord[0]] = 1
+
+    with open('display.txt', mode='w') as file_object:
+        print(display, file=file_object)
+
+
+    solution = len(coords) 
     print('\nSolution for first half!')
     print('SOLUTION DESCRIPTION: {}\n'.format(solution))
     return
@@ -56,7 +81,7 @@ def main():
     data = load_data() 
     coords, folds = process_data(data)
     first_half(coords, folds)
-    second_half(data)
+    second_half(coords, folds)
 
 
 if __name__ == '__main__':
