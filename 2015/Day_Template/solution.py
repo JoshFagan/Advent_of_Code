947d63170069e9e9db02e20d44f06e17ebabd6fa
Joshua Fagan <jfagan2@vols.utk.edu>
2022-11-10 11:44:37 -0500

    Adds template and starter for first day of 2015

---
+++ 2015/Day_Template/solution.py
@@ -0,0 +1,41 @@
+#!/Users/josh/.local/share/virtualenvs/Advent_of_Code-07aOslbA/bin/python3
+
+import csv
+import numpy as np
+
+
+def load_data():
+    # Read contents of file
+    with open('data.txt', 'r') as csvfile:
+        data = csv.reader(csvfile, delimiter='<DELIMITER>')
+        data = np.array([[value for value in row] for row in data])
+    return data
+
+
+def process_data(raw_data):
+    return 
+
+
+def first_half(input): 
+    solution = 'NOT DONE YET' 
+    print('\nSolution for first half!')
+    print('SOLUTION DESCRIPTION: {}\n'.format(solution))
+    return 
+        
+
+def second_half(input):
+    solution = 'NOT DONE YET' 
+    print('\nSolution for second half!')
+    print('SOLUTION DESCRIPTION: {}\n'.format(solution))
+    return
+
+
+def main():
+    data = load_data() 
+    process_data(data)
+    first_half(data)
+    second_half(data)
+
+
+if __name__ == '__main__':
+    main()
