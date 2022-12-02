0d05d6081cb01667f00a0e9bc28af9d69247caf0
Josh Fagan <jfagan2@lap324.nomad.utk.edu>
2022-12-02 11:29:07 -0500

    Final solution for day 2

--- 2022/Day_2/solution.py
+++ 2022/Day_2/solution.py
@@ -7,24 +7,32 @@ import numpy as np
 def load_data():
     # Read contents of file
     with open('data.txt', 'r') as csvfile:
-        data = csv.reader(csvfile, delimiter='<DELIMITER>')
+        data = csv.reader(csvfile, delimiter=' ')
         data = np.array([[value for value in row] for row in data])
     return data
 
 
-def process_data(raw_data):
-    return 
+def first_half(input):
+    mapping = {'A': {'X': 4, 'Y': 8, 'Z': 3}, 
+               'B': {'X': 1, 'Y': 5, 'Z': 9},
+               'C': {'X': 7, 'Y': 2, 'Z': 6}}
 
+    scores = [mapping[i[0]][i[1]] for i in input]
+    solution = sum(scores) 
 
-def first_half(input): 
-    solution = 'NOT DONE YET' 
     print('\nSolution for first half!')
     print('SOLUTION DESCRIPTION: {}\n'.format(solution))
     return 
         
 
 def second_half(input):
-    solution = 'NOT DONE YET' 
+    mapping = {'A': {'X':3 , 'Y': 4, 'Z': 8}, 
+               'B': {'X':1 , 'Y': 5, 'Z': 9},
+               'C': {'X':2 , 'Y': 6, 'Z': 7}}
+
+    scores = [mapping[i[0]][i[1]] for i in input]
+    solution = sum(scores) 
+    
     print('\nSolution for second half!')
     print('SOLUTION DESCRIPTION: {}\n'.format(solution))
     return
@@ -32,7 +40,6 @@ def second_half(input):
 
 def main():
     data = load_data() 
-    process_data(data)
     first_half(data)
     second_half(data)
 
