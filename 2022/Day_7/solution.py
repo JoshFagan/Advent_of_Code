8b71384c4325a93c110d1320c5e17d1f5e8bd666
Josh Fagan <jfagan2@lap324.local>
2022-12-08 13:19:30 -0500

    Creates solution for day 7

--- 2022/Day_7/solution.py
+++ 2022/Day_7/solution.py
@@ -10,10 +10,13 @@ class Directory:
         self.parent = parent
         self.children = []
         self.files = []
-        self._indent_string = indent_string
+        self.indent_string = indent_string
+        self.size = 0
 
     def __str__(self):
-        file_structure = f"{self._indent_string}- {self.name} (dir)\n"
+        file_structure = "{}- {} (dir, size={})\n".format(self.indent_string,
+                                                          self.name,
+                                                          self.size)
         for child in self.children:
             file_structure += f"{child}"
 
@@ -23,14 +26,50 @@ class Directory:
         return file_structure
 
 
+    def get_child(self, child_name):
+        for child in self.children:
+            if child.name == child_name:
+                return child
+    
+        new_child = Directory(name=child_name, 
+                              parent=self, 
+                              indent_string=self.indent_string+'  ')
+        self.children.append(new_child)
+    
+        return new_child
+
+
+    def add_file(self, file_name, file_size):
+        for file in self.files:
+            if file.name == file_name:
+                return
+                
+        new_file = File(name=file_name, 
+                        size=file_size, 
+                        indent_string=self.indent_string+'  ')
+        self.files.append(new_file)
+        self.update_size()
+        return
+
+
+    def update_size(self):
+        self.size = sum([file.size for file in self.files] +  
+                        [child.size for child in self.children])
+
+        if self.parent:
+            self.parent.update_size()
+
+
 class File:
     def __init__(self, name, size, indent_string):
         self.name = name
         self.size = size
-        self._indentation_string = indent_string
+        self.indent_string = indent_string
 
     def __str__(self):
-        return f"{self._indent_string}- {self.name} (file, size={self.size})\n"
+        return "{}- {} (file, size={})\n".format(self.indent_string,
+                                                 self.name,
+                                                 self.size)
 
 def load_data():
     # Read contents of file
@@ -47,20 +86,51 @@ def process_data(raw_data):
 
 def create_file_system(puzzle_input):
     root = Directory(name='/', parent=None, indent_string='')
+    curr_dir = root
+    for command in puzzle_input[1:]:
+        command = command.split(' ')
+        if command[1] == 'cd':
+            if command[-1] == '..':
+                curr_dir = curr_dir.parent
+            else:
+                curr_dir = curr_dir.get_child(command[-1])
+        elif command[0].isnumeric():
+            curr_dir.add_file(command[1], int(command[0]))
+        
     return root
 
 
-def first_half(puzzle_input): 
-    root = create_file_system(puzzle_input)
-    print(root)
-    solution = 'NOT DONE YET' 
+def get_dir_sizes(curr_dir):
+    dir_sizes = []
+    dir_sizes.append(curr_dir.size)
+
+    for child in curr_dir.children:
+        dir_sizes += get_dir_sizes(child)
+
+    return dir_sizes 
+
+
+def first_half(root): 
+    dir_sizes = get_dir_sizes(root) 
+    solution = sum([size for size in dir_sizes if size <= 100000])
     print('\nSolution for first half!')
     print('SOLUTION DESCRIPTION: {}\n'.format(solution))
     return 
         
 
-def second_half(puzzle_input):
-    solution = 'NOT DONE YET' 
+def second_half(root):
+    total_space = 70000000
+    needed_space = 30000000
+    used_space = root.size
+    free_space = total_space-used_space
+    extra_needed_space = needed_space - free_space
+
+    if extra_needed_space > 0:
+        dir_sizes = get_dir_sizes(root)
+        candidate_dir = min([size for size in dir_sizes 
+                             if size >= extra_needed_space])
+
+    solution = candidate_dir 
     print('\nSolution for second half!')
     print('SOLUTION DESCRIPTION: {}\n'.format(solution))
     return
@@ -68,9 +138,10 @@ def second_half(puzzle_input):
 
 def main():
     raw_data = load_data() 
-    data = process_data(raw_data)
-    first_half(data)
-    second_half(data)
+    root = create_file_system(raw_data)
+    print(root)
+    first_half(root)
+    second_half(root)
 
 
 if __name__ == '__main__':
