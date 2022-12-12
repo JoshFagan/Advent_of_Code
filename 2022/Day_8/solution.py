f65f14f15a17f8338a06bc3a5094afd7af33915b
Josh Fagan <jfagan2@lap324.local>
2022-12-12 16:39:27 -0500

    Sets up day 8

---
+++ 2022/Day_8/solution.py
@@ -0,0 +1,41 @@
+#!/Users/josh/.local/share/virtualenvs/Advent_of_Code-07aOslbA/bin/python3
+
+import csv
+import numpy as np
+
+
+def load_data():
+    # Read contents of file
+    with open('data.txt', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter='<DELIMITER>')
+        data = np.array([row[0] for row in reader])
+    return data
+
+
+def process_data(raw_data):
+    return raw_data 
+
+
+def first_half(puzzle_input): 
+    solution = 'NOT DONE YET' 
+    print('\nSolution for first half!')
+    print('SOLUTION DESCRIPTION: {}\n'.format(solution))
+    return 
+        
+
+def second_half(puzzle_input):
+    solution = 'NOT DONE YET' 
+    print('\nSolution for second half!')
+    print('SOLUTION DESCRIPTION: {}\n'.format(solution))
+    return
+
+
+def main():
+    raw_data = load_data() 
+    data = process_data(raw_data)
+    first_half(data)
+    second_half(data)
+
+
+if __name__ == '__main__':
+    main()
