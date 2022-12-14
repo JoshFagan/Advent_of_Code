3576c10db5f24ec2f41026e15d1aaae2acb19b3d
Josh Fagan <jfagan2@lap324.nomad.utk.edu>
2022-12-14 15:10:16 -0500

    Starts solution for Day 8

--- 2022/Day_8/solution.py
+++ 2022/Day_8/solution.py
@@ -1,4 +1,4 @@
-#!/Users/josh/.local/share/virtualenvs/Advent_of_Code-07aOslbA/bin/python3
+#!python
 
 import csv
 import numpy as np
@@ -7,8 +7,8 @@ import numpy as np
 def load_data():
     # Read contents of file
     with open('data.txt', 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter='<DELIMITER>')
-        data = np.array([row[0] for row in reader])
+        reader = csv.reader(csvfile)
+        data = np.array([ [int(c) for c in row[0]] for row in reader])
     return data
 
 
@@ -16,7 +16,20 @@ def process_data(raw_data):
     return raw_data 
 
 
+def is_visable(input_list):
+    max_matrix = np.zeros(input_list.shape)
+    max_matrix[0] = input_list[0]
+    for i in range(1, len(max_matrix)):
+        max_matrix[i] = max(input_list[i], max_matrix[i-1])
+
+    print(input_list)
+    print(max_matrix)
+
+
+
 def first_half(puzzle_input): 
+    rolling_max(puzzle_input[0])
+
     solution = 'NOT DONE YET' 
     print('\nSolution for first half!')
     print('SOLUTION DESCRIPTION: {}\n'.format(solution))
