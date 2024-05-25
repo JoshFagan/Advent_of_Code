99fd0c1b45dde5cb2929bbdb562e9ddf4b51b6fb
Joshua Fagan <josh@Joshuas-MacBook-Pro-7.local>
2024-05-24 21:29:32 -0400

    Progress on solution

--- 2021/Day_17/solution.py
+++ 2021/Day_17/solution.py
@@ -1,6 +1,9 @@
 #!/Users/josh/.local/share/virtualenvs/Advent_of_Code-07aOslbA/bin/python3
 
+import time
+
 import csv
+import numpy as np
 
 
 def load_data():
@@ -13,6 +16,24 @@ def load_data():
 
 def process_data(raw_data):
     return 
+
+
+def calc_trajectory_1(x_vel, y_vel, time_steps):
+    x = [0]
+    y = [0]
+
+    for i in range(time_steps):
+        x.append(x[i]+x_vel)
+        y.append(y[i]+y_vel)
+        x_vel = max(x_vel-1, 0)
+        y_vel -= 1
+
+
+def calc_trajectory_2(x_vel, y_vel, time_steps):
+    x = np.array(range(x_vel, x_vel-time_steps, -1))
+    x[x<0] = 0
+    x = np.cumsum(x)
+    y = np.cumsum(range(y_vel, y_vel-time_steps, -1))
 
 
 def first_half(input): 
@@ -30,10 +51,20 @@ def second_half(input):
 
 
 def main():
-    data = load_data() 
-    process_data(data)
-    first_half(data)
-    second_half(data)
+#    data = load_data() 
+#    process_data(data)
+#    first_half(data)
+#    second_half(data)
+
+    t1 = time.time()
+    calc_trajectory_2(7, 2, 100000)
+    t2 = time.time()
+
+    calc_trajectory_1(7, 2, 100000)
+    t3 = time.time()
+
+    print(t2-t1)
+    print(t3-t2)
 
 
 if __name__ == '__main__':
